fcc9bc2086eb3415c80b398145d4c9db4719db50
pragun-bansal <97455793+pragun-bansal@users.noreply.github.com>
2023-02-20 23:34:19 +0530

    snap-align added

--- src/App.jsx
+++ src/App.jsx
@@ -10,17 +10,22 @@ import FFtile from "./home/FFtile";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
+    <div className="App cont">
+      <section className="snap-align-top"><Navbar /> </section>
       <Announcement />
-      <Ftile />
-      <hr />
-      <Stile />
-      <hr />
-      <Ttile />
-      <hr />
-      <FFtile />
-      <Footer />
+      <section className="snap-align-top"><Ftile /> </section>
+      <section className="snap-align-top"><hr />
+      <Stile /></section>
+      <section className="snap-align-top"><hr />
+      <Ttile /> </section>
+      <section className="snap-align-top"><hr />
+      <FFtile /> </section>
+      <section className="snap-align-top"><Footer /> </section>
+      
+      
+      
+      
+      
     </div>
   );
 }
